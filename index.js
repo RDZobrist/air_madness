18ee4170af164dac5f09315ff1292f683635980b
Robert Zobrist <rdzobrist.dev@gmail.com>
2022-02-01 15:51:34 -0700

    added server to server static files

--- index.js
+++ index.js
@@ -11,3 +11,42 @@ app.listen(4005, () => { console.log('listening on port 4005') });
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
+app.get('/air-madness-empowerment-night', (req, res) => {
+    res.sendFile(path.join(__dirname, 'air-madness-empowerment-night.html'))
+});
+app.get('/attractions', (req, res) => {
+    res.sendFile(path.join(__dirname, 'attractions.html'))
+});
+app.get('/birthday-parties', (req, res) => {
+    res.sendFile(path.join(__dirname, 'birthday-parties.html'))
+});
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'))
+});
+app.get('/corporate-events', (req, res) => {
+    res.sendFile(path.join(__dirname, 'corporate-events.html'))
+});
+app.get('/cosmic-nights', (req, res) => {
+    res.sendFile(path.join(__dirname, 'cosmic-nights.html'))
+});
+app.get('/faqs', (req, res) => {
+    res.sendFile(path.join(__dirname, 'faqs.html'))
+});
+app.get('/laser-madness', (req, res) => {
+    res.sendFile(path.join(__dirname, 'laser-madness.html'))
+});
+app.get('/prices-and-gift-cards', (req, res) => {
+    res.sendFile(path.join(__dirname, 'prices-and-gift-cards.html'))
+});
+app.get('/private-parties', (req, res) => {
+    res.sendFile(path.join(__dirname, 'private-parties.html'))
+});
+app.get('/reviews', (req, res) => {
+    res.sendFile(path.join(__dirname, 'reviews.html'))
+});
+app.get('/trampoline-jumps', (req, res) => {
+    res.sendFile(path.join(__dirname, 'trampoline-jumps.html'))
+});
